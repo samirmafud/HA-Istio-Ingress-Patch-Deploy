ea773631620dd8b172a9636b4f7fcfa096f0ea22
Samir Alejandro Ruiz Mafud <samir.ruiz@mx.ey.com>
2024-03-26 00:18:50 -0600

    UPDATE:Actualizando las pruebas unitarias

--- test/istio_ingress_patch_test.py
+++ test/istio_ingress_patch_test.py
@@ -24,6 +24,25 @@ def terraform_output():
     yield tf.output()
     tf.destroy(tf_var_file="test-vars.tfvars",auto_approve=True)
 
+# Verifica el estado del Load Balancer
+def test_load_balancer_patch(terraform_output):
+    # Declara el nombre del Namespace
+    name_space = "istio-ingress"
+
+    # Declara el nombre del servicio
+    svc = "istio-ingressgateway"
+
+    # Ejecuta el comando para actualizar el kubeconfig del clúster de EKS
+    update_kubeconfig_cmd = f"aws eks update-kubeconfig --name {CLUSTER_NAME} --region {AWS_REGION} --profile {PROFILE}"
+    subprocess.run(update_kubeconfig_cmd, shell=True, check=True)
+
+    # Ejecuta el comando de kubectl para conocer el estado del Load Balancer
+    cmd = f"kubectl describe svc {svc} -n {name_space}"
+    result = subprocess.run(cmd, shell=True, text=True, capture_output=True)
+
+    # Verifica que el estado del Load balancer esté asegurado
+    assert "Ensured load balancer" in result.stdout, f"El Load Balancer en {name_space} no está corriendo. Salida de kubectl:\n{result.stdout}"
+
 # Verifica la instalación del parche en el Load Balancer
 def test_load_balancer_patch(terraform_output):
     # Declara el nombre del Namespace
@@ -40,5 +59,11 @@ def test_load_balancer_patch(terraform_output):
     cmd = f"kubectl describe svc {svc} -n {name_space}"
     result = subprocess.run(cmd, shell=True, text=True, capture_output=True)
 
-    # Verifica que el estado del Deployment sea "deployed" en la columna STATUS
-    assert "Ensured load balancer" in result.stdout, f"El Load Balancer en {name_space} no está corriendo. Salida de kubectl:\n{result.stdout}"
+    # Verifica que el parche se haya aplicado correctamente en los servicios indicados
+    assert "service.beta.kubernetes.io/aws-load-balancer-backend-protocol: tcp" in result.stdout, f"El Load Balancer en {name_space} no está corriendo. Salida de kubectl:\n{result.stdout}"
+    assert "service.beta.kubernetes.io/aws-load-balancer-connection-idle-timeout: 3600" in result.stdout, f"El Load Balancer en {name_space} no está corriendo. Salida de kubectl:\n{result.stdout}"
+    assert "service.beta.kubernetes.io/aws-load-balancer-internal: true" in result.stdout, f"El Load Balancer en {name_space} no está corriendo. Salida de kubectl:\n{result.stdout}"
+    assert "service.beta.kubernetes.io/aws-load-balancer-ssl-cert: arn:aws:acm:us-east-1:533267162190:certificate/5870c022-e6e6-4b86-ba80-299b7314be25" in result.stdout, f"El Load Balancer en {name_space} no está corriendo. Salida de kubectl:\n{result.stdout}"
+    assert "service.beta.kubernetes.io/aws-load-balancer-ssl-ports: 443" in result.stdout, f"El Load Balancer en {name_space} no está corriendo. Salida de kubectl:\n{result.stdout}"
+    assert "service.beta.kubernetes.io/aws-load-balancer-subnets: subnet-0fad6edde9b8cde59,subnet-0585bf6783ec9d761" in result.stdout, f"El Load Balancer en {name_space} no está corriendo. Salida de kubectl:\n{result.stdout}"
+    assert "service.beta.kubernetes.io/aws-load-balancer-type: nlb" in result.stdout, f"El Load Balancer en {name_space} no está corriendo. Salida de kubectl:\n{result.stdout}"
